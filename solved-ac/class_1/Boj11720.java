b1ea0332c2afbd5ba850391f7979a87f4dd970e4
giyun <dkwhkp0@naver.com>
2023-03-23 02:43:50 +0900

    boj 11720

---
+++ solved-ac/class_1/Boj11720.java
@@ -0,0 +1,11 @@
+package class_1;
+
+public class Boj11720 {
+    public static void main(String[] args) {
+        String line = "54321";
+        char[] chars = line.toCharArray();
+        for(char c : chars){
+            System.out.println(c);
+        }
+    }
+}
